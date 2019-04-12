1f2567a4eb2682ec7799c7df9793dc5a87657340
vijaymedi <48242340+vijaymedi@users.noreply.github.com>
2019-04-12 16:02:01 +0530

    more trailing space removal

--- create_json.py
+++ create_json.py
@@ -5,9 +5,9 @@ import cx_Oracle
 def create_json(feed):
 	dsn_tns = cx_Oracle.makedsn('35.243.168.164', '1521', service_name='pdborcl.us_east1_b.c.juniperonprem.internal')
 	conn = cx_Oracle.connect(user='micro', password='services', dsn=dsn_tns)
-	c=conn.cursor()	
+	c=conn.cursor()
 	feed_seq=feed.split('|')[0]
-	feed_name=feed.split('|')[1]	
+	feed_name=feed.split('|')[1]
 	c.execute("SELECT DISTINCT 																					\
 				LOWER(CONCAT(CONCAT(SUBSTR(pub_feed.pub_feed_name,INSTR(pub_feed.pub_feed_name,':')+1,LENGTH(pub_feed.pub_feed_name)-INSTR(pub_feed.pub_feed_name,':')-4),'.'),feed_dtls.tgt_tbl_name)) AS dataset_table,	\
 				LOWER(pub_feed.feed_src_bkt),																	\
@@ -30,12 +30,12 @@ def create_json(feed):
 		df = pd.DataFrame(data, columns = ['Dataset_Table', 'Bucket', 'Source_Table_Name'])
 		prepare_child_tag=""
 		for i in range(len(df)):
-		 prepare_child_tag=prepare_child_tag + "{'name': 'BQ:" + df['Dataset_Table'][i] + "','level':'orange','icon': '/assets/img/bigquery.png','children': [{'name': 'GCS:"+ df['Bucket'][i] + "','level':'blue','icon': '/assets/img/storage.png','children': [{'name': '"+ df['Source_Table_Name'][i] + "','level':'blue','icon': '/assets/img/source.png'}]}]},"		
+		 prepare_child_tag=prepare_child_tag + "{'name': 'BQ:" + df['Dataset_Table'][i] + "','level':'orange','icon': '/assets/img/bigquery.png','children': [{'name': 'GCS:"+ df['Bucket'][i] + "','level':'blue','icon': '/assets/img/storage.png','children': [{'name': '"+ df['Source_Table_Name'][i] + "','level':'blue','icon': '/assets/img/source.png'}]}]},"
 		childern_tags= prepare_child_tag[:-1]
-		json_final="[{'name':'Feed " + feed_name + "','icon':'/assets/img/feed.png','children': [" +  childern_tags + "]}]"				
+		json_final="[{'name':'Feed " + feed_name + "','icon':'/assets/img/feed.png','children': [" +  childern_tags + "]}]"	
 		temp_html = open('/home/ms/backend/lineage/template.html','r').read()
 		temp_html=str(temp_html)
-		temp_html = temp_html.replace('BIG_TREE',json_final)	
+		temp_html = temp_html.replace('BIG_TREE',json_final)
 		html=open('/home/ms/backend/lineage/index.html','w')
 		html.write(temp_html)
 		html.close()
